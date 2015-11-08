10225c0c78eb8c51ec48377b4091815a01584010
kaimou1357 <kaimou1357@gmail.com>
2015-11-08 17:28:10 -0500

    Enums Added

--- app/src/main/java/urlinq/android/com/edu_chat/model/ECMessageType.java
+++ app/src/main/java/urlinq/android/com/edu_chat/model/ECMessageType.java
@@ -4,12 +4,19 @@ package urlinq.android.com.edu_chat.model;
  * Created by kaimou on 11/3/15.
  */
 public enum ECMessageType {
-    ALMessageTextType,
-    ALMessageFileType,
-    ALMessageEventType,
-    AlTypingType,
-    ALCommentType,
-    ALNotSupportedType
+    ALMessageTextType(0),
+    ALMessageFileType(1),
+    ALMessageEventType(2),
+    AlTypingType(3),
+    ALCommentType(4),
+    ALNotSupportedType(5);
+
+    private final int value;
+    ECMessageType(final int newValue){
+        value = newValue;
+    }
+    public int getValue(){return value;}
+
 
 
 }
