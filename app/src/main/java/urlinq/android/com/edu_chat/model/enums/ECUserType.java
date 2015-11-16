445e934c090f71b8247bcee7e91729917c680e3c
kaimou1357 <kaimou1357@gmail.com>
2015-11-15 19:41:36 -0500

    Strings to Enums.

--- app/src/main/java/urlinq/android/com/edu_chat/model/enums/ECUserType.java
+++ app/src/main/java/urlinq/android/com/edu_chat/model/enums/ECUserType.java
@@ -4,6 +4,13 @@ package urlinq.android.com.edu_chat.model.enums;
  * Created by Jacob on 10/29/15.
  */
 public enum ECUserType {
-	ECUserTypeStudent,
-	ECUserTypeProfessor
+	ECUserTypeStudent("User"),
+	ECUserTypeProfessor("User");
+
+	private final String userType;
+
+
+	ECUserType(final String userType){this.userType = userType;}
+
+	public String getUserType(){return userType;}
 }
