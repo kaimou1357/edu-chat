2a8a01863d434e34dc5e434795b651b390874719
Jacob Niebloom <niebloomj@gmail.com>
2015-11-24 17:53:00 -0500

    Removed Kai's inter-manager-controller dependency flaw. Contains errors now.

--- app/src/main/java/urlinq/android/com/edu_chat/manager/ECApiManager.java
+++ app/src/main/java/urlinq/android/com/edu_chat/manager/ECApiManager.java
@@ -1,24 +1,24 @@
 package urlinq.android.com.edu_chat.manager;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Looper;
 import android.util.Log;
-import com.loopj.android.http.*;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.PersistentCookieStore;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.SyncHttpClient;
 import com.parse.ParseInstallation;
 import com.parse.ParseObject;
-import cz.msebera.android.httpclient.Header;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import urlinq.android.com.edu_chat.controller.ChatActivity;
-import urlinq.android.com.edu_chat.controller.MainActivity;
+import java.text.ParseException;
+
+import cz.msebera.android.httpclient.Header;
 import urlinq.android.com.edu_chat.model.ECMessage;
 import urlinq.android.com.edu_chat.model.ECUser;
-
-import java.text.ParseException;
 
 
 /**
@@ -26,116 +26,120 @@ import java.text.ParseException;
  */
 public class ECApiManager {
 
-	public static final String loginAPI = "https://edu.chat/api/login/";
+    public static final String loginAPI = "https://edu.chat/api/login/";
     public static final String loadoutAPI = "https://edu.chat/message/loadout";
     public static final String sendMessageURL = "https://edu.chat/message/send/";
     public static final String loadChatRoomURL = "https://edu.chat/message/load_chat";
 
 
-	// A SyncHttpClient is an AsyncHttpClient
-	private static final AsyncHttpClient syncHttpClient = new SyncHttpClient();
-	private static final AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+    // A SyncHttpClient is an AsyncHttpClient
+    private static final AsyncHttpClient syncHttpClient = new SyncHttpClient();
+    private static final AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
 
-	public static void setCookieStore(PersistentCookieStore cookieStore) {
-		getClient().setCookieStore(cookieStore);
-	}
-
-
-	public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-		getClient().get(url, params, responseHandler);
-	}
-
-	public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-		getClient().post(url, params, responseHandler);
-	}
-
-	/**
-	 * @return an async client when calling from the main thread, otherwise a sync client.
-	 */
-	private static AsyncHttpClient getClient() {
-		// Return the synchronous HTTP client when the thread is not prepared
-		if (Looper.myLooper() == null)
-			return syncHttpClient;
-		return asyncHttpClient;
-	}
-
-	private static class AllECApiCalls {
-		private String url;
-		private RequestParams params;
-
-		public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-
-		}
-
-		public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-			allAPICallErrorHandler(statusCode, headers, responseBody, error);
-		}
-
-		public void onFinish() {
-
-		}
-
-		public static void allAPICallErrorHandler(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-			error.printStackTrace();
-			Log.e(ECApiManager.class.getSimpleName(), "^ ^ ^ An ECApiManager call has failed!!");
-		}
-
-		public void invokePost() {
-			ECApiManager.post(url, params, new AsyncHttpResponseHandler() {
-
-				@Override
-				public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-					AllECApiCalls.this.onSuccess(statusCode, headers, responseBody);
-				}
+    public static void setCookieStore(PersistentCookieStore cookieStore) {
+        getClient().setCookieStore(cookieStore);
+    }
 
 
-				@Override
-				public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-					AllECApiCalls.this.onFailure(statusCode, headers, responseBody, error);
-				}
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        getClient().get(url, params, responseHandler);
+    }
 
-				@Override
-				public void onFinish() {
-					AllECApiCalls.this.onFinish();
-				}
-			});
-		}
-		public void invokeGet(){
-			ECApiManager.get(url, params, new AsyncHttpResponseHandler() {
-				@Override
-				public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-					AllECApiCalls.this.onSuccess(statusCode, headers, responseBody);
-				}
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        getClient().post(url, params, responseHandler);
+    }
 
-				@Override
-				public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-					AllECApiCalls.this.onFailure(statusCode, headers, responseBody, error);
-				}
-				@Override
-				public void onFinish(){AllECApiCalls.this.onFinish();}
-			});
-		}
-	}
+    /**
+     * @return an async client when calling from the main thread, otherwise a sync client.
+     */
+    private static AsyncHttpClient getClient() {
+        // Return the synchronous HTTP client when the thread is not prepared
+        if (Looper.myLooper() == null)
+            return syncHttpClient;
+        return asyncHttpClient;
+    }
+
+    private static class AllECApiCalls {
+        private String url;
+        private RequestParams params;
+        public JSONObject obj;
+
+        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+        }
+
+        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+            allAPICallErrorHandler(statusCode, headers, responseBody, error);
+        }
+
+        public void onFinish() {
+
+        }
+
+        public static void allAPICallErrorHandler(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+            error.printStackTrace();
+            Log.e(ECApiManager.class.getSimpleName(), "^ ^ ^ An ECApiManager call has failed!!");
+        }
+
+        public void invokePost() {
+            ECApiManager.post(url, params, new AsyncHttpResponseHandler() {
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    AllECApiCalls.this.onSuccess(statusCode, headers, responseBody);
+                }
+
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                    AllECApiCalls.this.onFailure(statusCode, headers, responseBody, error);
+                }
+
+                @Override
+                public void onFinish() {
+                    AllECApiCalls.this.onFinish();
+                }
+            });
+        }
+
+        public void invokeGet() {
+            ECApiManager.get(url, params, new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    AllECApiCalls.this.onSuccess(statusCode, headers, responseBody);
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                    AllECApiCalls.this.onFailure(statusCode, headers, responseBody, error);
+                }
+
+                @Override
+                public void onFinish() {
+                    AllECApiCalls.this.onFinish();
+                }
+            });
+        }
+    }
 
 
     /**
      * This class will load update the chatroom with new messages as soon as the user enters the chat room.
      */
-    public static class LoadChatMessageObject extends AllECApiCalls{
-        private JSONArray obj;
+    public static class LoadChatMessageObject extends AllECApiCalls {
         private String userHash;
-        private ChatActivity activity;
-        public LoadChatMessageObject(RequestParams params, ChatActivity context){
+
+        public LoadChatMessageObject(RequestParams params) {
             super.params = params;
             super.url = loadChatRoomURL;
-            this.activity = context;
         }
+
         @Override
         public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
             super.onSuccess(statusCode, headers, responseBody);
             userHash = new String(responseBody);
             try {
-                obj = new JSONObject(userHash).getJSONArray("messages");
+                super.obj = new JSONObject(userHash).getJSONObject("messages");
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -158,72 +162,70 @@ public class ECApiManager {
      */
     public static class LoginObject extends AllECApiCalls {
 
-		private JSONObject obj;
-		private String userHash;
+        private String userHash;
 
-		public LoginObject(RequestParams params) {
-			super.params = params;
-			super.url = loginAPI;
-		}
+        public LoginObject(RequestParams params) {
+            super.params = params;
+            super.url = loginAPI;
+        }
 
-		@Override
-		public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-			super.onSuccess(statusCode, headers, responseBody);
-			userHash = new String(responseBody);
-			Log.d("login", userHash);
-			try {
-				obj = new JSONObject(userHash);
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-		}
+        @Override
+        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+            super.onSuccess(statusCode, headers, responseBody);
+            userHash = new String(responseBody);
+            Log.d("login", userHash);
+            try {
+                super.obj = new JSONObject(userHash);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
 
-		@Override
-		public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-			super.onFailure(statusCode, headers, responseBody, error);
-		}
+        @Override
+        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+            super.onFailure(statusCode, headers, responseBody, error);
+        }
 
-		@Override
-		public void onFinish() {
-			super.onFinish();
+        @Override
+        public void onFinish() {
+            super.onFinish();
 
-			try {
-				ECUser.setCurrentUser(new ECUser(obj.getJSONObject("user")));
-				ECUser.setUserToken(obj.getString("token"));
-				ECUser.setCurrentUserSchool(obj.getJSONObject("user").getJSONObject("school").getString("school_name"));
+            try {
+                ECUser.setCurrentUser(new ECUser(obj.getJSONObject("user")));
+                ECUser.setUserToken(obj.getString("token"));
+                ECUser.setCurrentUserSchool(obj.getJSONObject("user").getJSONObject("school").getString("school_name"));
 
-				ParseInstallation.getCurrentInstallation().saveInBackground();
-				ParseObject login = new ParseObject("Logins");
-				login.put("userid", ECUser.getCurrentUser().getObjectIdentifier());
-				login.put("OS", "Android");
-				login.saveInBackground();
+                ParseInstallation.getCurrentInstallation().saveInBackground();
+                ParseObject login = new ParseObject("Logins");
+                login.put("userid", ECUser.getCurrentUser().getObjectIdentifier());
+                login.put("OS", "Android");
+                login.saveInBackground();
 
-			} catch (ParseException | JSONException e) {
-				e.printStackTrace();
-			}
+            } catch (ParseException | JSONException e) {
+                e.printStackTrace();
+            }
 
 
-		}
-	}
+        }
+    }
 
     /**
      * This class will build and populate each Recyclerview in the application's MainActivity.
      */
-    public static class MainLoadOutObject extends AllECApiCalls{
-        private JSONObject obj;
+    public static class MainLoadOutObject extends AllECApiCalls {
         private String userHash;
-        private MainActivity activity;
-        public MainLoadOutObject(RequestParams params, MainActivity context){
+
+        public MainLoadOutObject(RequestParams params) {
             super.params = params;
             super.url = loadoutAPI;
-            this.activity = context;
         }
+
         @Override
         public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
             super.onSuccess(statusCode, headers, responseBody);
             userHash = new String(responseBody);
             try {
-                obj = new JSONObject(userHash);
+                super.obj = new JSONObject(userHash);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -246,21 +248,20 @@ public class ECApiManager {
     /**
      * This class will send messages for the user.
      */
-    public static class SendMessageObject extends AllECApiCalls{
-        private JSONObject obj;
+    public static class SendMessageObject extends AllECApiCalls {
         private String userHash;
-        private ChatActivity activity;
-        public SendMessageObject(RequestParams params, ChatActivity context){
+
+        public SendMessageObject(RequestParams params) {
             super.params = params;
             super.url = sendMessageURL;
-            this.activity = context;
         }
+
         @Override
         public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
             super.onSuccess(statusCode, headers, responseBody);
             userHash = new String(responseBody);
             try {
-                obj = new JSONObject(userHash);
+                super.obj = new JSONObject(userHash);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -274,9 +275,9 @@ public class ECApiManager {
         @Override
         public void onFinish() {
             super.onFinish();
-            try{
+            try {
                 activity.addMessage(new ECMessage(obj.getJSONObject("message")));
-            }catch (ParseException | JSONException e) {
+            } catch (ParseException | JSONException e) {
                 e.printStackTrace();
             }
 
