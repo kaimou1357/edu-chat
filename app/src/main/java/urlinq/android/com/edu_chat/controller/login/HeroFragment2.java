6e3f020c57b18f5c120a07113ea7734f57746984
kaimou1357 <kaimou1357@gmail.com>
2015-11-30 00:24:04 -0500

    ViewPager added back in.

---
+++ app/src/main/java/urlinq/android/com/edu_chat/controller/login/HeroFragment2.java
@@ -0,0 +1,21 @@
+package urlinq.android.com.edu_chat.controller.login;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import urlinq.android.com.edu_chat.R;
+
+/**
+ * Created by Kai on 9/7/2015.
+ */
+public class HeroFragment2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.welcome_fragment2, container, false);
+        return v;
+
+    }
+}
--- app/src/main/java/urlinq/android/com/edu_chat/controller/login/LoginFragment.java
+++ app/src/main/java/urlinq/android/com/edu_chat/controller/login/LoginFragment.java
@@ -1,8 +1,14 @@
 package urlinq.android.com.edu_chat.controller.login;
 
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +18,9 @@ import urlinq.android.com.edu_chat.R;
  * Created by Kai on 9/4/2015.
  */
 public class LoginFragment extends Fragment {
-//    int numberOfViewPagerChildren = 3;
-//    int lastIndexOfViewPagerChildren = numberOfViewPagerChildren;
-//    //private ViewPager mViewPager;
-//    //private PagerAdapter mCustomPagerAdapter;
+    int numberOfViewPagerChildren = 3;
+    int lastIndexOfViewPagerChildren = numberOfViewPagerChildren;
+    private ViewPager mViewPager;
 
 
 	@Override
@@ -25,87 +30,84 @@ public class LoginFragment extends Fragment {
 	 */
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		//initialize viewpager object.
+        View v = inflater.inflate(R.layout.login_main, container, false);
+
+        mViewPager = (ViewPager) v.findViewById(R.id.loginPager);
+        mViewPager.setAdapter(new LoginAdapter(getActivity().getSupportFragmentManager()));
 
 
-		/**
-		 * Problems with Bitmap memory allocation in SDK 22
-		 */
-//        mViewPager = (ViewPager) v.findViewById(R.id.loginPager);
-//        mViewPager.setAdapter(new LoginAdapter(getActivity().getSupportFragmentManager()));
-//
-//
-//        final LayerDrawable background = (LayerDrawable) mViewPager.getBackground();
-//
-//        background.getDrawable(0).setAlpha(0);
-//        //lowest drawable
-//        background.getDrawable(1).setAlpha(0);
-//        background.getDrawable(2).setAlpha(1); //outermost drawable.
-//
-//        mViewPager.setPageTransformer(true, new ViewPager.PageTransformer() {
-//            @Override
-//            public void transformPage(View view, float position) {
-//                int index = (Integer) view.getTag();
-//                Drawable currentDrawableInLayerDrawable;
-//                currentDrawableInLayerDrawable = background.getDrawable(index);
-//                // Change the visibility of each of the fragment layers when swiping left and right.
-//
-//                if (position <= -1 || position >= 1) {
-//                    currentDrawableInLayerDrawable.setAlpha(0);
-//                } else if (position == 0) {
-//                    currentDrawableInLayerDrawable.setAlpha(255);
-//                } else {
-//                    currentDrawableInLayerDrawable.setAlpha((int) (255 - Math.abs(position * 255)));
-//                }
-//            }
-//        });
+        final LayerDrawable background = (LayerDrawable) mViewPager.getBackground();
 
-		return inflater.inflate(R.layout.login_main, container, false);
+        background.getDrawable(0).setAlpha(0);
+        //lowest drawable
+        background.getDrawable(1).setAlpha(0);
+        background.getDrawable(2).setAlpha(1); //outermost drawable.
+
+        mViewPager.setPageTransformer(true, new ViewPager.PageTransformer() {
+            @Override
+            public void transformPage(View view, float position) {
+                int index = (Integer) view.getTag();
+                Drawable currentDrawableInLayerDrawable;
+                currentDrawableInLayerDrawable = background.getDrawable(index);
+                // Change the visibility of each of the fragment layers when swiping left and right.
+
+                if (position <= -1 || position >= 1) {
+                    currentDrawableInLayerDrawable.setAlpha(0);
+                } else if (position == 0) {
+                    currentDrawableInLayerDrawable.setAlpha(255);
+                } else {
+                    currentDrawableInLayerDrawable.setAlpha((int) (255 - Math.abs(position * 255)));
+                }
+            }
+        });
+
+		return v;
 	}
 
 
 	/**
 	 * Adapter created for the ViewPager to flip through all the fragments.
 	 */
-//    class LoginAdapter extends FragmentStatePagerAdapter {
-//        public LoginAdapter(FragmentManager fm) {
-//            super(fm);
-//        }
-//
-//        @Override
-//        public Fragment getItem(int i) {
-//            Fragment fragment = null;
-//            if (i == 0) {
-//                fragment = new HeroFragment1();
-//            }
-//            if (i == 1) {
-//                fragment = new HeroFragment2();
-//            }
-//            if (i == 2) {
-//                fragment = new HeroFragment3();
-//            }
-//            return fragment;
-//
-//        }
-//
-//        @Override
-//        public int getCount() {
-//            return numberOfViewPagerChildren;
-//        }
-//
-//        @Override
-//        public boolean isViewFromObject(View view, Object object) {
-//            if (object instanceof HeroFragment1) {
-//                view.setTag(2);
-//            }
-//            if (object instanceof HeroFragment2) {
-//                view.setTag(1);
-//            }
-//            if (object instanceof HeroFragment3) {
-//                view.setTag(0);
-//            }
-//            return super.isViewFromObject(view, object);
-//        }
-//    }
+    class LoginAdapter extends FragmentStatePagerAdapter {
+        public LoginAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            Fragment fragment = null;
+            if (i == 0) {
+                fragment = new HeroFragment1();
+            }
+            if (i == 1) {
+                fragment = new HeroFragment2();
+            }
+            if (i == 2) {
+                fragment = new HeroFragment3();
+            }
+            return fragment;
+
+        }
+
+        @Override
+        public int getCount() {
+            return numberOfViewPagerChildren;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            if (object instanceof HeroFragment1) {
+                view.setTag(2);
+            }
+            if (object instanceof HeroFragment2) {
+                view.setTag(1);
+            }
+            if (object instanceof HeroFragment3) {
+                view.setTag(0);
+            }
+            return super.isViewFromObject(view, object);
+        }
+    }
 
 
 }
