5fc53d63c6cf69265a34c2988366610845ae4923
Jacob Niebloom <niebloomj@gmail.com>
2016-02-08 10:54:42 -0500

    Instabug Upgrade

--- app/src/main/java/urlinq/android/com/edu_chat/controller/login/LoginActivity.java
+++ app/src/main/java/urlinq/android/com/edu_chat/controller/login/LoginActivity.java
@@ -1,26 +1,26 @@
 package urlinq.android.com.edu_chat.controller.login;
 
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
-
-import butterknife.Bind;
 import butterknife.ButterKnife;
+import com.instabug.library.Feature;
+import com.instabug.library.IBGInvocationEvent;
 import com.instabug.library.Instabug;
-import com.instabug.wrapper.support.activity.InstabugAppCompatActivity;
-import com.parse.Parse;
-import com.urlinq.edu_chat.BuildConfig;
 import com.urlinq.edu_chat.R;
 import urlinq.android.com.edu_chat.controller.MainActivity;
 
-public class LoginActivity extends InstabugAppCompatActivity implements LoginBackground.OnLoginListener {
+public class LoginActivity extends AppCompatActivity implements LoginBackground.OnLoginListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		Instabug.initialize(getApplication(), "4719bf1a7d10635f7c1520989f084d4c");
+		new Instabug.Builder(this.getApplication(), "4719bf1a7d10635f7c1520989f084d4c")
+				.setInvocationEvent(IBGInvocationEvent.IBGInvocationEventShake)
+				.setDebugEnabled(true)
+				.setInstabugLogState(Feature.State.ENABLED)
+				.build();
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.content_main);
 		ButterKnife.bind(this);
